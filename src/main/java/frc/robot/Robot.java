df117384ccf9239bf463fa2e5550e446da073e43
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-04-11 09:19:04 -0400

    Force WPILib Hal Simulator to default to Blue1 Alliance

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -142,7 +142,11 @@ public class Robot extends LoggedRobot {
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+    edu.wpi.first.wpilibj.simulation.DriverStationSim.setAllianceStationId(
+        edu.wpi.first.hal.AllianceStationID.Blue1);
+    edu.wpi.first.wpilibj.simulation.DriverStationSim.notifyNewData();
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
