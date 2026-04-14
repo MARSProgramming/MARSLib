368937e6b9bf019f6dbebcf9665ba6dbbaec3c0c
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-04-13 20:31:44 -0400

    fix: enforce Cloudflare NodeJS compat V2 date in root wrangler file to resolve actual prerender engine crash

--- website/src/pages/api/keystatic/[...params].ts
+++
@@ -1,5 +0,0 @@
-import { makeAPIRoute } from '@keystatic/astro/api';
-import keystaticConfig from '../../../../keystatic.config';
-
-export const prerender = false;
-export const all = makeAPIRoute(keystaticConfig);
