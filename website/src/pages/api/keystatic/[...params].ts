002ec03a9f9b5fec9f067167b0725c2888a08828
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-04-13 23:53:40 -0400

    fix(cms): uppercase ALL export for astro 6 API route handler compatibility

--- website/src/pages/api/keystatic/[...params].ts
+++ website/src/pages/api/keystatic/[...params].ts
@@ -2,4 +2,4 @@ import { makeHandler } from '@keystatic/astro/api';
 import config from '../../../../keystatic.config';
 
 export const prerender = false;
-export const all = makeHandler({ config });
+export const ALL = makeHandler({ config });
